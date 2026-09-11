d3e747d31fbbc7195bba9c39473a16209fe5a7d6
popking159 <47087407+popking159@users.noreply.github.com>
2026-09-12 02:36:03 +0300

    Update myinstaller.sh

--- myinstaller.sh
+++ myinstaller.sh
@@ -7,7 +7,7 @@
 
 PLUGIN_NAME="CustomChannelSelection"
 PKG_BASE="enigma2-plugin-extensions-customchannelselection"
-VERSION="1.6"
+VERSION="2.0"
 USERNAME="popking159"
 REPO="customchannelselection"
 
